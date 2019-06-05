ad31f0d9a45086ce540a6050372ccc57e26181b6
Tuan Pham <tuanqup@gmail.com>
2019-06-05 14:57:53 +0200

    Removing hard coded imagesurl

--- aksgrant.sh
+++ aksgrant.sh
@@ -4,7 +4,7 @@
 AKS_RESOURCE_GROUP=DevDaysOslo2019
 AKS_CLUSTER_NAME=aksdevdays
 ACR_RESOURCE_GROUP=DevDaysOslo2019
-ACR_NAME=acrdevdays
+ACR_NAME=acrdevdaystp
 
 # Get the id of the service principal configured for AKS
 CLIENT_ID=$(az aks show --resource-group $AKS_RESOURCE_GROUP --name $AKS_CLUSTER_NAME --query "servicePrincipalProfile.clientId" --output tsv)
